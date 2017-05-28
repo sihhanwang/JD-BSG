acf91abbb29f3b7f49dd78f95262d5f0ac2ed905
NewMax <zhangpengmax@gmail.com>
2017-05-27 22:48:37 -0400

    define catergory as 1

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'aasm'
 gem 'gritter'
 gem "figaro"
 gem "fog"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,6 +6,7 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
   def show
     @product = Product.find(params[:id])
   end
@@ -18,6 +19,7 @@ class Admin::ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     @product.category_id = params[:category_id]
+    @product.category_id = 1
     if @product.save
       redirect_to admin_products_path
     else
@@ -27,7 +29,7 @@ class Admin::ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
-    @categories = Category.all.order("category_group_id, name")
+    @categories = Category.all
   end
 
   def update
