68fdec2c9f1376b43267b29cdfca4ef08d0f7bd0
sihhan <thehappyend95@gmail.com>
2017-05-29 23:58:22 +0800

    fix error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,35 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    # 商品類型 / 品牌
-    @category_groups = CategoryGroup.published
-    #收藏
-    if params[:favorite] == "yes"
-      @products = current_user.products
-    end
-    # 判斷是否篩選分類
-    if params[:category].present?
-      @category_s = params[:category]
-      @category = Category.find_by(name: @category_s)
-
-      @products = Product.where(:category => @category.id).published.recent.paginate(:page => params[:page], :per_page => 12)
-
-    # 判斷是否篩選類型
-    elsif params[:group].present?
-      @group_s = params[:group]
-      @group = CategoryGroup.find_by(name: @group_s)
-
-      @products = Product.joins(:category).where("categories.category_group_id" => @group.id).published.recent.paginate(:page => params[:page], :per_page => 12)
-
-      # 預設顯示所有公開商品
-      else
-        @products = Product.all
-    end
+    @products = Product.all
   end
 
   def show
     @product = Product.find(params[:id])
-    @category_groups = CategoryGroup.published
 
   end
 
