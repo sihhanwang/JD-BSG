ef08126b98f116496fdda3896b5cb6d4ef125ea8
NewMax <zhangpengmax@gmail.com>
2017-06-08 22:48:37 -0400

    adjust_navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524150607) do
+ActiveRecord::Schema.define(version: 20170521065121) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -23,23 +23,6 @@ ActiveRecord::Schema.define(version: 20170524150607) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.text     "desc"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.integer  "category_group_id"
-    t.boolean  "is_hidden",         default: true
-  end
-
-  create_table "category_groups", force: :cascade do |t|
-    t.string   "name"
-    t.text     "desc"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.boolean  "is_hidden",  default: true
   end
 
   create_table "favorites", force: :cascade do |t|
